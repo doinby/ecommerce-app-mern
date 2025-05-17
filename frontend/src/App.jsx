b1e01662f00a5037e2b70ca3458d0734a51a10a1
KC Do <contact@doinby.co>
2025-05-17 14:33:31 +1000

    added react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './components/Header';
 import Product from './components/Product';
+import { Routes, Route } from 'react-router';
 
 export default function App() {
 	return (
 		<>
+			<Routes>
+				<Route path='login' element={} />
+			</Routes>
 			<Header />
 			<main className='mx-[5%] flex flex-col justify-center align-center'>
 				<Product />
