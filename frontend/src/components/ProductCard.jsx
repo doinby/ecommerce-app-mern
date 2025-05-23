6015008bc39c78a1fca6fa09f520e886ff932701
KC Do <contact@doinby.co>
2025-05-23 12:32:42 +1000

    add review count to product card

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -12,17 +12,20 @@ export default function ProductCard({ product }) {
 		price,
 		// countInStock,
 		rating,
-		// numReviews,
+		numReviews,
 	} = product;
 
 	return (
 		<Link
 			to={`/${category}/${_id}`}
-			className='w-48 p-2 rounded-sm border-1 border-slate-300'
+			className='w-48 p-2 space-y-3.5 rounded-sm border-1 border-slate-300'
 		>
-			<p>{name}</p>
 			<img className='' src={image} />
-			<Rating rating={rating} />
+			<p>{name}</p>
+			<div className='flex justify-between items-center'>
+				<Rating rating={rating} />
+				<p className='text-sm'>{`${numReviews} Reviews`}</p>
+			</div>
 			<p>${price}</p>
 		</Link>
 	);
