f2eaf3f655e220fd458b554028ae92ca889f8e5b
KC Do <contact@doinby.co>
2025-06-01 17:07:58 +1000

    add console log db host and name

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 export default async function connectDB() {
 	try {
 		const conn = await mongoose.connect(process.env.DB_URI);
-		console.log(`MongoDB Connected ${conn.connection.host}`);
+		console.log(`connected to host: ${conn.connection.host}`);
+		console.log(`current db name: ${conn.connection.name}`);
 	} catch (error) {
 		console.log(`Error: ${error.message}`);
 		process.exit(1);
